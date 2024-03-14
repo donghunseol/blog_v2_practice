b5c2d453996367b99535ada808c517eff715643e
seoldonghun <ehdgns5647@gmail.com>
2024-03-14 16:35:24 +0900

    환경세팅 완료

---
+++ src/test/java/shop/coding/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.coding.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
