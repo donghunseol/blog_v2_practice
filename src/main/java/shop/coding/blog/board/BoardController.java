f68204818351149bac674770ae30b1c54c8bdb70
seoldonghun <ehdgns5647@gmail.com>
2024-03-14 17:26:36 +0900

    게시글 목록보기

--- src/main/java/shop/coding/blog/board/BoardController.java
+++ src/main/java/shop/coding/blog/board/BoardController.java
@@ -1,10 +1,13 @@
 package shop.coding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,7 +16,9 @@ public class BoardController {
     private final BoardNativeRepository boardNativeRepository;
 
     @GetMapping("/" )
-    public String index() {
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardNativeRepository.findAll();
+        request.setAttribute("boardList", boardList);
         return "index";
     }
 
---
+++ src/test/java/shop/coding/blog/board/BoardNativeRepositoryTest.java
@@ -0,0 +1,31 @@
+package shop.coding.blog.board;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+@Import(BoardNativeRepository.class)
+@DataJpaTest
+public class BoardNativeRepositoryTest {
+    @Autowired
+    private BoardNativeRepository boardNativeRepository;
+
+    @Test
+    public void findAll_test(){
+        // given
+
+        // when
+        List<Board> boardList = boardNativeRepository.findAll();
+        System.out.println("findAll_test/size : " + boardList.size());
+        System.out.println("findAll_test/First/username : " + boardList.getFirst().getUsername());
+
+        // then
+        Assertions.assertThat(boardList.size()).isEqualTo(4);
+        Assertions.assertThat(boardList.getFirst().getUsername()).isEqualTo("love");
+    }
+}
